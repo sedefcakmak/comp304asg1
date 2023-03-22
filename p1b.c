4d275a546e6a840f0a8b38a97681a3a8dd096db6
sedefcakmak <86186809+sedefcakmak@users.noreply.github.com>
2023-03-22 21:10:18 +0300

    Add files via upload

---
+++ p1b.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+
+//question 1-b
+
+
+
+
+void zombie_process() {
+    //forking one child process
+    pid_t pid = fork();
+
+    if (pid == 0) { //for child process
+        sleep(8); //for program to be remain at least 5 seconds
+        wait(NULL);
+        exit(0);
+
+    }
+    kill(pid, SIGSEGV);
+
+    printf("Parent: Child process changed to Zombie process");
+
+}
+
+int main(int argc, char *argv[]) {
+
+    zombie_process();
+
+}
+
+
